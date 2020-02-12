b872441bba069f74e515fa34aa040aacd4e659da
makewheels <33256842+makewheels@users.noreply.github.com>
2020-02-12 15:57:27 +0800

    init

---
+++ src/test/java/com/eg/spiderlistenrequest/SpiderListenRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.spiderlistenrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderListenRequestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
